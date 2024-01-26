02b745af81099e787ac751f8c43e9d71e322c094
marely <marely.carcamo@gmail.com>
2024-01-26 04:34:42 -0300

    comentarios

--- validaRut.py
+++ validaRut.py
@@ -4,10 +4,8 @@ import re
 #Esta función valida el formato de ingreso de datos
 def validarDato(dato):
     patron = r'^\d{1,8}-[\dKk]$'
-    if re.match(patron, dato):
-        return True
-    else:
-        return False
+    return re.match(patron, dato) # retorna un valor booleano
+
 
 #Esta función valida el rut, ingresando como parámetros el número y el digito verificador
 def validarRut(texto,dvRut):
@@ -28,11 +26,12 @@ def validarRut(texto,dvRut):
     else:
         dv = str(d)
         print((dv), (dvRut))
-    return dv == dvRut
+    return dv == dvRut    #Retorna valor booleano
 
 
 #PROGRAMA VALIDADOR DE RUT CHILENO
 
+#Bloque principal
 while True:
     dato = input("Ingrese rut: ")
     if validarDato(dato):
